bc51780fd03861595f02be14befc2dafed6c7b6d
akanksha-telang <aakankshatelang@gmail.com>
2025-10-04 20:45:33 +0530

    Initial React project commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import CurrentLocation from "./currentLocation";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="container">
+        <CurrentLocation />
+      </div>
+      <div className="footer-info">
+        
+        
+        <a target="_blank" href="https://www.AkankshaTelang.dev/">
+          Akanksha Telang
+        </a>{" "}
+        | Powered by{" "}
+        <a target="_blank" href="https://www.htmlhints.com/">
+          HTML HINTS
+        </a>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "c20bd96cf2f6a13a8948daa8c5306447",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
