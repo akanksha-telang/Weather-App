062cfb7a6913d9b7db200df9a9823fe2078e4b34
akanksha-telang <aakankshatelang@gmail.com>
2025-10-06 19:48:49 +0530

    Moved project files to root and cleaned repo

---
+++ build/precache-manifest.35f30c545699ef93c77b321afacf0923.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b50c60f0f9f0a3bd84f4b4d3463a049d",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "ce89615356f74a03c013",
+    "url": "/Weather-App/static/css/main.03ba2023.chunk.css"
+  },
+  {
+    "revision": "6dcfc77f0f18ba424a25",
+    "url": "/Weather-App/static/js/2.701fe897.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Weather-App/static/js/2.701fe897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce89615356f74a03c013",
+    "url": "/Weather-App/static/js/main.41a7a4e5.chunk.js"
+  },
+  {
+    "revision": "43c75a2350d7767095b8",
+    "url": "/Weather-App/static/js/runtime-main.147888a5.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/Weather-App/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "75e0f8ca422777deab777d3abe0a90e7",
+    "url": "/Weather-App/static/media/background.75e0f8ca.jpg"
+  },
+  {
+    "revision": "e1698332435d6a6fb48294f85157aeab",
+    "url": "/Weather-App/static/media/city.e1698332.jpg"
+  }
+]);
